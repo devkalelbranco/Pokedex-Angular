dd5ebaaf956abb2785c43eb85ea964ed04b35f3d
devkalelbranco <dev.kalel.branco@gmail.com>
2023-08-11 18:48:08 -0300

    feat: consulta de nomes

--- src/app/service/api-pokemon.service.ts
+++ src/app/service/api-pokemon.service.ts
@@ -6,10 +6,18 @@ import { Pokemon } from '../components/models/pokemon';
   providedIn: 'root'
 })
 export class ApiPokemonService {
+<<<<<<< Updated upstream
   pokemons:Array<Pokemon> = [];
+=======
+  pokemons: any = [];
+  types = [];
+  names:Array<string> = [];
+  
+>>>>>>> Stashed changes
 
   constructor( private httpClient:HttpClient) {
     this.getPokemons();
+    this.getNames();
    }
   async getPokemons() {
     const request = await this.httpClient.get<any>(' https://pokeapi.co/api/v2/pokemon?limit=30').toPromise();
@@ -17,6 +25,18 @@ export class ApiPokemonService {
     this.pokemons = request.results;
     console.log(this.pokemons);
         
+  }
+
+  getNames(){
+    const request = this.httpClient.get<any>('https://pokeapi.co/api/v2/pokemon?limit=1010').toPromise();
+    this.names = [];
+    request.then((item) => {
+      if(item.results){
+        item.results.forEach((item:any) => {
+          this.names.push(item.name);
+        })
+      }
+    }).finally(() => console.log(this.names));
   }
 }
  
